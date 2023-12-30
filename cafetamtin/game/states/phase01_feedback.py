b4bf71bddfe8929ae67811079d1a0503f82874af
Robertino Mendes Santiago Junior <robertino@ufpr.br>
2023-12-30 17:26:55 -0300

    ajuste na herança das phases

--- cafetamtin/game/states/phase01_feedback.py
+++ cafetamtin/game/states/phase01_feedback.py
@@ -28,7 +28,6 @@ from game import WHITE, BLACK, RED, GREEN, YELLOW
 
 from base.board import Board
 from game.states.state import State
-from utils.confetti import Confetti
 from game.actors.teacher import Teacher
 from game.actors.student import Student
 from production.type_error import TypeError
@@ -43,9 +42,7 @@ class Phase01Feedback(State):
         self.rules = Phase01Levels(self.memory)
         self.board = Board(self.game.app)
         self.teacher = Teacher(self.game.game_canvas)
-        self.confetti = Confetti()
-        self.frame_confetti = 1
-                
+                        
         self.box_width, self.box_height = 60, 60
         
         self.show_error_misinterpretation_language = False
@@ -124,16 +121,6 @@ class Phase01Feedback(State):
         self.memory.add_fact('reset_timer', True)
     
         super().exit_state()
-        
-    def draw_confetti(self):
-        display = self.game.game_canvas
-        screen_width, screen_height = self.game.GAME_WIDTH, self.game.GAME_HEIGHT
-        frame = self.confetti.get_image(self.frame_confetti)
-        frame_rect = frame.get_rect(center=(screen_width/2, screen_height/2 - 80))
-        display.blit(frame, frame_rect)
-        self.frame_confetti += 1
-        if self.frame_confetti > self.confetti.total_frames:
-            self.confetti.visible = False
     
     def draw_physical_buttons(self):
         display = self.game.game_canvas
@@ -146,42 +133,6 @@ class Phase01Feedback(State):
         pygame.draw.circle(display,RED,(130,baseline_circle),10)
         red_text = font.render("Continuar" if (self.show_teacher and self.teacher.has_next_message()) else "Fechar", True, (0,0,0))
         display.blit(red_text, (145, baseline_text))
-
-    def blit_text(self, text, pos, color, size):
-        display = self.game.game_canvas
-        font = pygame.font.SysFont(FONT_NAME, size, False, False)
-        text = font.render(text, True, color)
-        text_rect = text.get_rect(center=(pos[0], pos[1]))
-        display.blit(text, text_rect)
-        
-    def blit_box(self, text, pos, color, size):
-        display = self.game.game_canvas
-        rect = (pos[0], pos[1], self.box_width, self.box_height)
-        shape = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
-        pygame.draw.rect(shape, color, shape.get_rect())
-        display.blit(shape, rect)
-        self.blit_text(text, (pos[0]+self.box_width/2, pos[1]+self.box_height/2), WHITE, size)
-
-    def draw_arrow_right(self, message, pos, angle, color, size):
-        display = self.game.game_canvas
-        font = pygame.font.SysFont('DejaVu Sans', size, False, False)
-        text = font.render(f'{message}'+u' \u2192', True, color)
-        text = pygame.transform.rotate(text, angle)
-        text_rect = text.get_rect(topright=pos)
-        display.blit(text, text_rect)
-    
-        
-    def draw_arrow_left(self, message, pos, angle, color, size):
-        display = self.game.game_canvas
-        font = pygame.font.SysFont('DejaVu Sans', size, False, False)
-        text = font.render(u'\u2190 '+f'{message}', True, color)
-        text = pygame.transform.rotate(text, angle)
-        text_rect = text.get_rect(topleft=pos)
-        display.blit(text, text_rect)
-        
-    def draw_circle(self, pos, radius, color):
-        display = self.game.game_canvas
-        pygame.draw.circle(display,color,pos,radius)
         
     def draw_error_uncategorized_solution(self):
         display = self.game.game_canvas
@@ -740,17 +691,6 @@ class Phase01Feedback(State):
             user = user,
             session = session
         )
-    
-    
-    def draw_confetti(self):
-        display = self.game.game_canvas
-        screen_width, screen_height = self.game.GAME_WIDTH, self.game.GAME_HEIGHT
-        frame = self.confetti.get_image(self.frame_confetti)
-        frame_rect = frame.get_rect(center=(screen_width/2, screen_height/2 - 80))
-        display.blit(frame, frame_rect)
-        self.frame_confetti += 1
-        if self.frame_confetti > self.confetti.total_frames:
-            self.confetti.visible = False
         
     def render(self, display):
         font = pygame.font.SysFont(FONT_NAME, 20, False, False)
--- cafetamtin/game/states/state.py
+++ cafetamtin/game/states/state.py
@@ -18,10 +18,16 @@
 import pygame
 from gpiozero import Button
 
+from game import FONT_NAME
+from game import WHITE, BLACK, RED, GREEN, YELLOW
+from utils.confetti import Confetti
+
 class State():
     def __init__(self, game):
         self.game = game
         self.previous_state = None
+        self.confetti = Confetti()
+        self.frame_confetti = 1
 
     def update(self, delta_time):
         pass
@@ -40,3 +46,49 @@ class State():
     def exit_state(self, stages = 1):
         for i in range(0, stages):
             self.game.state_stack.pop()
+            
+    def blit_text(self, text, pos, color, size):
+        display = self.game.game_canvas
+        font = pygame.font.SysFont(FONT_NAME, size, False, False)
+        text = font.render(text, True, color)
+        text_rect = text.get_rect(center=(pos[0], pos[1]))
+        display.blit(text, text_rect)
+        
+    def blit_box(self, text, pos, color, size):
+        display = self.game.game_canvas
+        rect = (pos[0], pos[1], self.box_width, self.box_height)
+        shape = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+        pygame.draw.rect(shape, color, shape.get_rect())
+        display.blit(shape, rect)
+        self.blit_text(text, (pos[0]+self.box_width/2, pos[1]+self.box_height/2), WHITE, size)
+
+    def draw_arrow_right(self, message, pos, angle, color, size):
+        display = self.game.game_canvas
+        font = pygame.font.SysFont('DejaVu Sans', size, False, False)
+        text = font.render(f'{message}'+u' \u2192', True, color)
+        text = pygame.transform.rotate(text, angle)
+        text_rect = text.get_rect(topright=pos)
+        display.blit(text, text_rect)
+    
+        
+    def draw_arrow_left(self, message, pos, angle, color, size):
+        display = self.game.game_canvas
+        font = pygame.font.SysFont('DejaVu Sans', size, False, False)
+        text = font.render(u'\u2190 '+f'{message}', True, color)
+        text = pygame.transform.rotate(text, angle)
+        text_rect = text.get_rect(topleft=pos)
+        display.blit(text, text_rect)
+        
+    def draw_circle(self, pos, radius, color):
+        display = self.game.game_canvas
+        pygame.draw.circle(display,color,pos,radius)
+        
+    def draw_confetti(self):
+        display = self.game.game_canvas
+        screen_width, screen_height = self.game.GAME_WIDTH, self.game.GAME_HEIGHT
+        frame = self.confetti.get_image(self.frame_confetti)
+        frame_rect = frame.get_rect(center=(screen_width/2, screen_height/2 - 80))
+        display.blit(frame, frame_rect)
+        self.frame_confetti += 1
+        if self.frame_confetti > self.confetti.total_frames:
+            self.confetti.visible = False
